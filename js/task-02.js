953441d525074c087d39609dd974dc95c2a10305
Igor-Korniichuk <kiv160780@gmail.com>
2022-04-21 13:04:39 +0300

    Выполняет задание 2.

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ingredientsAll = [];
+// for (const ingredient of ingredients) {
+//   const ingredientEl = document.createElement('li');
+//   ingredientEl.classList.add('item');
+//   ingredientEl.textContent = ingredient;
+//   ingredientsAll.push(ingredientEl);  
+// }
+
+// const ingredientsUl = document.querySelector('#ingredients');
+// ingredientsUl.append(...ingredientsAll);
+
+const ingredientsAll = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+        ingredientEl.classList.add('item');
+        ingredientEl.textContent = ingredient;
+        return ingredientEl;
+})
+        const ingredientsUl = document.querySelector('#ingredients');
+        ingredientsUl.append(...ingredientsAll);
