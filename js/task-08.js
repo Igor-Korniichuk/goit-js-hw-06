e1b026cd650eac3abc53c7fc75752ed50d413c76
Igor-Korniichuk <kiv160780@gmail.com>
2022-04-26 19:42:13 +0300

    Выполняет задание 8.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('form.login-form');
+const inputEmailEl = document.querySelector('input[type="email"]');
+const inputPusswordEl = document.querySelector('input[type="password"]');
 
+formEl.addEventListener('submit', onHendlerForm);
+
+function onHendlerForm(event) {
+    event.preventDefault();    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formElementsItems = {
+        email,
+        password,
+    };
+    
+    if (email === '' || password === '') {
+        window.alert("Все поля должны быть заполнены!");
+    }
+    console.log(formElementsItems);
+    formEl.reset();
+}
